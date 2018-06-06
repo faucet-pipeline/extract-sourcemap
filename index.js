ecc2c52e987da97e9c7f1d7a95180c4a9e15ef71
Lucas Dohmen <lucas@dohmen.io>
2018-06-06 16:57:41 +0200

    Just an extraction from faucet-pipeline

---
+++ index.js
@@ -0,0 +1,15 @@
+let { readFileSync } = require("fs");
+let regex = /sourceMappingURL=data:application\/json;base64,(\S+)/;
+
+module.exports = (fileName, attributes) => {
+	let result = regex.exec(readFileSync(fileName));
+	if(!result) {
+		throw new Error("No Source Map found");
+	}
+	let sourceMapBuffer = Buffer.from(result[1], "base64");
+	let sourceMap = JSON.parse(sourceMapBuffer);
+	Object.keys(sourceMap).
+		filter(key => !attributes.includes(key)).
+		forEach(key => delete sourceMap[key]);
+	return JSON.stringify(sourceMap);
+};
